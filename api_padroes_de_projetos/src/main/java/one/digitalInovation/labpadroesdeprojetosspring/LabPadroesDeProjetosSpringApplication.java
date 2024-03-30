a4f0affea43ee73093916d5ed84b2e29bbb7c1b9
Luar <luarrezende@gmail.com>
2024-03-30 16:04:16 -0300

    add pasta initializr

---
+++ api_padroes_de_projetos/src/main/java/one/digitalInovation/labpadroesdeprojetosspring/LabPadroesDeProjetosSpringApplication.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.labpadroesdeprojetosspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesDeProjetosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesDeProjetosSpringApplication.class, args);
+	}
+
+}
---
+++ api_padroes_de_projetos/src/test/java/one/digitalInovation/labpadroesdeprojetosspring/LabPadroesDeProjetosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalInovation.labpadroesdeprojetosspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPadroesDeProjetosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
